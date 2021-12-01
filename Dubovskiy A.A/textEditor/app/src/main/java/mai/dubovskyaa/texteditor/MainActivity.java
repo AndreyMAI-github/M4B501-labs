46483f3d590292ffd7e30e29192234b93efaf9db
AndreyMAI-github <andrey.dubovsky@gmail.com>
2021-12-01 19:04:46 +0300

    Hello world

--- Dubovskiy A.A/textEditor/app/src/main/java/mai/dubovskyaa/texteditor/MainActivity.java
+++ Dubovskiy A.A/textEditor/app/src/main/java/mai/dubovskyaa/texteditor/MainActivity.java
@@ -3,12 +3,17 @@ package mai.dubovskyaa.texteditor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView helloView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloView = findViewById(R.id.hello_view);
+        helloView.setText("This is my view");
     }
 }
