6cfbdb76c7e4522f29d0f451c12d2b1b83ddce1b
cha0xfox <psicho0202@gmail.com>
2021-12-12 00:45:53 +0300

    Пофиксил отображение названий файлов

--- SkvortsovIM/app/src/main/java/mai/skvortsovim/texteditor/MainActivity.java
+++ SkvortsovIM/app/src/main/java/mai/skvortsovim/texteditor/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
@@ -106,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
                 fileNames.add(f.getName());
         }
 
-        notepad.setText(fileNames.toString().replaceAll("\\[|\\]",""));
+        notepad.setText("Files \nFileName:\n" + fileNames.toString().replaceAll("\\[|\\]",""));
 
     };
 
